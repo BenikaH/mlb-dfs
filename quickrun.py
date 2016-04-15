cb3b085ef2981a11de70bc93149fdaef2da45de7
MurrDogg4 <rm643dplay@gmail.com>
2016-04-15 15:45:05 -0400

    20160415 update

---
+++ quickrun.py
@@ -0,0 +1,15 @@
+#!/usr/local/bin/python2.7
+import pinnaclemlb
+import starting_lineups
+import get_outputs
+
+def main():
+
+    pinnaclemlb.main()
+    starting_lineups.main()
+    get_outputs.main()
+
+    return
+
+if __name__ == '__main__':
+    main()
